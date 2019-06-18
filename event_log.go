34c82a559ff4aad9f9c28c3de15333ce90ffe1f1
EnigmaQuip <enigmaquip@gmail.com>
2019-06-18 15:32:18 -0600

    Some id's getting resent when they shouldn't...

--- event_log.go
+++ event_log.go
@@ -26,8 +26,10 @@ func (e *EventLog) Clear() {
 
 // Replay events to a subscriber
 func (e *EventLog) Replay(s *Subscriber) {
+	eventid, _ := strconv.Atoi(s.eventid)
 	for i := 0; i < len((*e)); i++ {
-		if string((*e)[i].ID) >= s.eventid {
+		thisid, _ := strconv.Atoi(string((*e)[i].ID))
+		if thisid > eventid {
 			s.connection <- (*e)[i]
 		}
 	}
